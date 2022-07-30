6905e07b2999bcddb8f19efbfe529a88db08f7a5
Marcos Paulo <32870576+MarcosPaulo775@users.noreply.github.com>
2022-07-30 16:35:59 -0300

    Search (#4)

--- anota-ai-test-frontend/src/app/components/search-input/search-input.component.ts
+++ anota-ai-test-frontend/src/app/components/search-input/search-input.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-search-input',
   templateUrl: './search-input.component.html',
-  styleUrls: ['./search-input.component.css']
+  styleUrls: ['./search-input.component.css'],
 })
 export class SearchInputComponent implements OnInit {
+  @Input() searchInput!: FormControl<string>;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
